ab7857c2b86482c392e9b70f7fc6b699b629841c
Antoine CORDIER <acordier@chopin.local>
2022-01-06 09:15:51 +0100

    refactor: parallel mvn install in gvt-switch

--- .zsh/gvt.zsh
+++ .zsh/gvt.zsh
@@ -11,25 +11,34 @@ assert_directory_is_clean() {
     fi
 }
 
-gvt-apim-switch() {
-    APIM_DIR="${APIM_DIR:-$HOME/src/gravitee/gravitee-api-management}"
-    VERSION="$1"
-
+switch_branch() {
     echo "Switching ref to ${VERSION}"
 
     pushd "${APIM_DIR}"
         assert_directory_is_clean
         git checkout "${VERSION}"
     popd
+}
 
+install_rest_api() {
     pushd "${APIM_DIR}/gravitee-apim-rest-api"
         mvn clean install -DskipTests
     popd
+}
 
+install_gateway() {
     pushd "${APIM_DIR}/gravitee-apim-gateway"
         mvn clean install -DskipTests
     popd
+}
 
+clean() {
+    pushd "${APIM_DIR}"
+        git checkout .
+    popd
+}
+
+package_repository() {
     pushd "${APIM_DIR}/gravitee-apim-repository"
         mvn clean package -DskipTests
         
@@ -44,8 +53,15 @@ gvt-apim-switch() {
         cp "${MONGO_PLUGIN}" "${API_PLUGINS}"
         cp "${MONGO_PLUGIN}" "${GTW_PLUGINS}"
     popd
+}
 
-    pushd "${APIM_DIR}"
-        git checkout .
-    popd
+gvt-apim-switch() {
+    APIM_DIR="${APIM_DIR:-$HOME/src/gravitee/gravitee-api-management}"
+    VERSION="$1"
+
+    install_rest_api & install_gateway
+
+    package_repository
+
+    clean
 }
